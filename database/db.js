67b8cfb8663c613a39e6b21a90088d0c117da5cb
aphosura4ever <stefan.denis03@yahoo.com>
2020-01-03 13:43:11 +0200

    first commit

---
+++ database/db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "qwertyhnjkl",
+    database: "login"
+  });
+  
+  
+  
+  connection.connect(err => {
+    if (err) console.error("Error connection to database");
+    else console.log("Database connected");
+  });
+  
+
+  module.exports = connection
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const session = require('express-session')
+const cors = require('cors')
+
+//const connection = require('./database/db')
+const passport = require('./passport');
+const app = express();
+const PORT = 8080
+// Route requires
+const route = require('./routes/route');
+
+// MIDDLEWARE
+
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+
+// Sessions
+app.use(
+	session({
+		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+	
+		resave: false, //required
+		saveUninitialized: false //required
+	})
+)
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
+
+
+// Routes
+app.use('/user', route)
+
+// Starting Server 
+app.listen(PORT, () => {
+	console.log(`App listening on PORT: ${PORT}`)
+})
